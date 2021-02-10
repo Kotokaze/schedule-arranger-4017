c04bf3d44531540fdcfa281bcfb68b2d920a6e6c
Kotokaze <5r1j6lvag@relay.firefox.com>
2021-02-10 13:18:06 +0900

    update

--- app.js
+++ app.js
@@ -7,6 +7,29 @@ var helmet = require('helmet');
 var session = require('express-session');
 var passport = require('passport');
 
+// モデルの読み込み
+var User = require('./models/user');
+var Schedule = require('./models/schedule');
+var Availability = require('./models/availability');
+var Candidate = require('./models/candidate');
+var Comment = require('./models/comment');
+User.sync().then(() => {
+
+  Schedule.belongsTo(User, { foreignKey: 'createdBy' });
+  Schedule.sync();
+
+  Comment.belongsTo(User, { foreignKey: 'userId' });
+  Comment.sync();
+
+  Availability.belongsTo(User, { foreignKey: 'userId' });
+
+  Candidate.sync().then(() => {
+
+    Availability.belongsTo(Candidate, { foreignKey: 'candidateId' });
+    Availability.sync();
+  });
+});
+
 var GitHubStrategy = require('passport-github2').Strategy;
 var GITHUB_CLIENT_ID = '2f831cb3d4aac02393aa';
 var GITHUB_CLIENT_SECRET = '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
@@ -20,15 +43,27 @@ passport.deserializeUser(function (obj, done) {
 });
 
 
-passport.use(new GitHubStrategy({
+passport.use(new GitHubStrategy(
+  {
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
   callbackURL: 'http://localhost:8000/auth/github/callback'
-},
+  },
+
   function (accessToken, refreshToken, profile, done) {
-    process.nextTick(function () {
-      return done(null, profile);
-    });
+
+    process.nextTick(
+
+      function () {
+
+        User.upsert(
+          {
+            userId: profile.id,
+            username: profile.username
+          }
+        ).then(() => { done(null, profile); });
+      }
+    );
   }
 ));
 
